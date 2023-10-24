14480775fceb93e9fad55a4fb130a8ac13ff6210
Karlossam <daniskate98@gmail.com>
2023-10-24 10:13:26 +0000

    Completamos ejercicio 3

---
+++ ex3/my_ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int age = 10;
+	int height = 2;
+
+	printf("I'm %d years old.\n", argv);
+	printf("I'm %d meters tall.\n", height);
+
+	return 0;
+}
