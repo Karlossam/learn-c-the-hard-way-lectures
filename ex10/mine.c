7c52250bfc10f8cd4e42797613d4e8c12de0e390
Karlossam <daniskate98@gmail.com>
2023-11-08 09:46:05 +0000

    Terminamos el ex 10

--- ex10/mine.c
+++ ex10/mine.c
@@ -1,43 +1,24 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
+	if (argc < 2) {
 		printf("ERROR: not enought arguments\n");
 		return 1;
 	}
+	int o = 1;
+	for (o=1; o < argc; o++) {
+		int i = 0;
+		for (i=0; argv[o][i] != '\0'; i++) {
+			char letter = argv[1][i];
+			if (letter < 97) {
+				letter += 30;
+			}
 
-	int i = 0;
-	for (i=0; argv[1][i] != '\0'; i++) {
-		char letter = argv[1][i];
-
-		switch (-1) {
-			case 'a':
-			case 'A':
-				printf("%d: 'A'\n", i);
-				break;
-
-			case 'e':
-			case 'E':
-				printf("%d: 'E'\n", i);
-				break;
-
-			case 'i':
-			case 'I':
-				printf("%d: 'I'\n", i);
-				break;
-
-			case 'o':
-			case 'O':
-				printf("%d: 'O'\n", i);
-				break;
-
-			case 'u':
-			case 'U':
-				printf("%d: 'U'\n", i);
-				break;
-
-			default:
+			if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+				printf("%d is a vowel\n", i);
+			} else {
 				printf("%d: %c is not a vowel\n", i, argv[1][i]);
+			}
 		}
 	}
 }
